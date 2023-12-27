d90b74594fbbc0197be46dab49511f6beaf20f61
JackYang <pgrammer.ybj@outlook.com>
2023-12-27 17:12:05 +0800

    Create Android plugin

---
+++ example/android/app/src/main/java/com/mengtu/saywo/authenticate_tencent_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mengtu.saywo.authenticate_tencent_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/AuthenticateTencentPlugin.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AuthenticateTencentPlugin : NSObject<FlutterPlugin>
+@end
+
+
+NS_ASSUME_NONNULL_END
